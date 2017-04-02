542decbfa481e4ad8970beee47e163080f330afb
root <root@localhost.localdomain>
2017-04-02 15:05:34 -0500

    testing0

--- Network.cpp
+++ Network.cpp
@@ -38,6 +38,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <Network.h>
+#include <Queue.h>
+#include <Packet.h>
 
 //Default Constructor
 void Network::Network() {}
--- Network.h
+++ Network.h
@@ -37,7 +37,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <Logger.h>
 #include <RFM69.h>
 #include <SPI.h>
 #ifndef Network_h
